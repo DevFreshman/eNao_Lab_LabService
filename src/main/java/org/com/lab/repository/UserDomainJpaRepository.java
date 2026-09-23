278c47c3ad0e5c3e48c7adc81084a8a3f3a770aa
Hoàng Đức Mạnh <hoangducmanh2004789@gmail.com>
2026-09-23 15:32:41 +0700

    fix query

--- src/main/java/org/com/lab/repository/UserDomainJpaRepository.java
+++ src/main/java/org/com/lab/repository/UserDomainJpaRepository.java
@@ -2,10 +2,13 @@ package org.com.lab.repository;
 
 import org.com.lab.entity.UserDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserDomainJpaRepository extends JpaRepository<UserDomain, Integer> {
-    List<UserDomain> findByUserIdAndRevokedAtNull(String userId);
-    List<Long> findDomainIdsByUserIdAndRevokedAtNull(String userId);
+
+    @Query("SELECT u.domainId FROM UserDomain u WHERE u.userId = :userId AND u.revokedAt IS NULL")
+    List<Long> findDomainIdsByUserIdAndRevokedAtNull(@Param("userId") String userId);
 }
